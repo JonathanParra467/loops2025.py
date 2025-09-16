bba3ac1efeb5d80d5b154a0aadfd1886090d5793
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-09-16 17:35:56 -0500

    Add files via upload

--- loops.py
+++ loops.py
@@ -1,4 +1,6 @@
-
+"""
+Write a Python program to find and print all numbers divisible by 7 between 1 and 300. Use a for loop and the modulus operator (%).
+"""
 for i in range(7, 300, 7):
     if i % 1 == 7:
         continue
